4b60f72109f0d2e5d1e0163cbafa70e49094fbc4
8thgencore <axelmaker@ya.ru>
2024-01-08 15:42:20 +0300

    ✨ Init project

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="app-footer">{{ stats.active }} more to do, {{ stats.done }} done</footer>
+</template>
+<script lang="ts">
+import IStats from "@/types/Stats";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    stats: {
+      type: Object as PropType<IStats>,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ src/types/Stats.ts
@@ -0,0 +1,4 @@
+export default interface IStats {
+  active: number;
+  done: number;
+}
